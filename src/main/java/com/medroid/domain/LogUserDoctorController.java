05a0c3f74e54aea05ebdb13cc3faab275bc49137
staticTJO <sk8terboy6931@hotmail.com>
2016-02-20 21:08:46 -0600

    Added Json For Login Tables

--- src/main/java/com/medroid/domain/LogUserDoctorController.java
+++ src/main/java/com/medroid/domain/LogUserDoctorController.java
@@ -2,9 +2,11 @@ package com.medroid.domain;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 
 @RequestMapping("/loguserdoctors")
 @Controller
 @RooWebScaffold(path = "loguserdoctors", formBackingObject = LogUserDoctor.class)
+@RooWebJson(jsonObject = LogUserDoctor.class)
 public class LogUserDoctorController {
 }
--- src/main/java/com/medroid/domain/LogUserPatientController.java
+++ src/main/java/com/medroid/domain/LogUserPatientController.java
@@ -2,9 +2,11 @@ package com.medroid.domain;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 
 @RequestMapping("/loguserpatients")
 @Controller
 @RooWebScaffold(path = "loguserpatients", formBackingObject = LogUserPatient.class)
+@RooWebJson(jsonObject = LogUserPatient.class)
 public class LogUserPatientController {
 }
